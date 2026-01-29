01a2acfff3b21f14bbe0fab9ef1e8b5de69ff9e7
mahutt <tommy.mahut@gmail.com>
2026-01-29 13:25:15 -0500

    feat: add mapbox map

---
+++ README.md
@@ -0,0 +1,16 @@
+# conuspots
+
+Conuspots is an searchable, interactive map meant to help students find classrooms and other campus locations.
+
+### Setup Instructions
+
+1. Clone this repository
+2. Install dependencies with `npm install`
+3. Add a mapbox access token to a local `.env` file like so:
+
+```
+VITE_MAPBOX_ACCESS_TOKEN=pk.123...
+```
+
+4. Run the local development server with `npm run dev`
+5. Visit http://localhost:5173/
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import mapboxgl from 'mapbox-gl'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+if (!MAPBOX_ACCESS_TOKEN) {
+  throw new Error('Missing Mapbox access token')
+}
+mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
+
+const mapContainer = document.getElementById('map')
+if (!mapContainer) {
+  throw new Error('Missing map container element')
+}
+
+const map = new mapboxgl.Map({
+  container: mapContainer,
+  center: [-74.5, 40],
+  style: 'mapbox://styles/mapbox/standard',
+  zoom: 9,
+})
+
+map.on('style.load', () => {
+  map.setConfigProperty('basemap', 'theme', 'monochrome')
+})
